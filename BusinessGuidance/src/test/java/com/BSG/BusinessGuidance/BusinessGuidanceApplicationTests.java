bd0071ac04c1ae218583e77d39dd7ca798cd8236
ArdaKarakaş <arda.karakas1234@gmail.com>
2022-12-18 01:34:31 +0300

    third commit

---
+++ BusinessGuidance/src/test/java/com/BSG/BusinessGuidance/BusinessGuidanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BSG.BusinessGuidance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessGuidanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
